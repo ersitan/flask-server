a7e195dd526e626d72c796b55e99d235ca7e5069
Ersin Tanriverdi <ersitan@gmail.com>
2023-12-15 22:45:07 +0100

    small modifications

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route('/')
+def hello():
+    return "Hey Flask"
+
+
+if __name__ == '__main__':
+    app.run(host="192.168.2.25", debug=True)
