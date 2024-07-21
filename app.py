90e3c252f397864a11e8f4791472705c359e01c1
Ersin Tanriverdi@ndus3 <ersitan@gmail.com>
2024-07-21 23:09:38 +0200

    linked href and getting posts

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, request, jsonify, render_template
+from werkzeug.exceptions import abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -38,6 +39,14 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 @app.route('/')
 def index():
@@ -45,6 +54,11 @@ def index():
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
 
 
 #
