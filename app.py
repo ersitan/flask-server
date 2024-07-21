ed0c258a40e29c9460faba91b936c63c1616df61
Ersin Tanriverdi@Home <ersitan@gmail.com>
2024-07-22 00:59:56 +0200

    added edit delete and create functions

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import sqlite3
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ASEITGJDFKLGDNSFGPAE]RJGTDL;FKGJAP@#$123%$zCDXV/'
 
 
 #
@@ -39,6 +40,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+
 def get_post(post_id):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?',
@@ -48,6 +50,7 @@ def get_post(post_id):
         abort(404)
     return post
 
+
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -55,11 +58,63 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
 
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
 
 #
 #
